5628ed432ed52588faad3965f5dab33b39e77e60
Matheus Camerini <matheuscameriniberto@gmail.com>
2026-08-05 11:21:15 -0300

    balbla

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,9 @@
+import image from './assets/image.png'
+function Paragrafo (){
+    return <p>
+        Parágrafo explicativo
+        <br></br>
+        <img src={image} width={290}/>
+    </p>
+}   
+export default Paragrafo
